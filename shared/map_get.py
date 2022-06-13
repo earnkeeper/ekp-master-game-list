954af685124188c169aa1b414b73ca9e2589c1f3
gavin-shaw <gavin@gnarus.io>
2022-06-13 13:27:58 +0200

    fix: missing websites in coingecko sync

--- shared/map_get.py
+++ shared/map_get.py
@@ -2,6 +2,13 @@ def map_get(map, keys):
     value = map
     
     for key in keys:
+        if type(key) == int:
+            if type(value) != list:
+                return None
+            if key >= len(value):
+                return None
+            return value[key]
+        
         if key not in value:
             return None
         
