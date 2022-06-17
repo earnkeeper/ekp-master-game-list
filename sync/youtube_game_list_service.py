74b97e0b03710c40c1ad6fca35770023ee5ef534
Hikmat <hikmet.pirmamedov@mail.ru>
2022-06-17 12:18:48 +0400

    refactor: refactor the methods

--- sync/youtube_game_list_service.py
+++ sync/youtube_game_list_service.py
@@ -6,9 +6,9 @@ from youtubesearchpython import *
 
 class YoutubeSyncService:
     def __init__(
-        self,
-        game_repo: GameRepo,
-        youtube_repo: YoutubeRepo,
+            self,
+            game_repo: GameRepo,
+            youtube_repo: YoutubeRepo,
     ):
         self.game_repo = game_repo
         self.youtube_repo = youtube_repo
@@ -22,28 +22,25 @@ class YoutubeSyncService:
 
             self.youtube_repo.save(videos)
 
-
-
     async def get_youtube_game_videos_info(self, game_name):
         videos_list = VideosSearch(game_name, limit=10).result()['result']
         videos = []
         for video in videos_list:
-            print(video)
-            document = {
-                "id": video['id'],
-                "game_name": game_name,
-                "title": video['accessibility']['title'],
-                "video_description": video['descriptionSnippet'][0]['text'] if video['descriptionSnippet'] else None,
-                "thumbnail": video['thumbnails'][0]['url'],
-                "view_count": video['viewCount']['text'],
-                "duration": video['duration'],
-                "publish_time": video['publishedTime'],
-                "link": video['link']
-
-            }
-
+            document = await self.get_single_video_info(video, game_name)
             videos.append(document)
 
         return videos
 
+    async def get_single_video_info(self, video, game_name):
+        return {
+            "id": video['id'],
+            "game_name": game_name,
+            "title": video['accessibility']['title'],
+            "video_description": video['descriptionSnippet'][0]['text'] if video['descriptionSnippet'] else None,
+            "thumbnail": video['thumbnails'][0]['url'],
+            "view_count": video['viewCount']['text'],
+            "duration": video['duration'],
+            "publish_time": video['publishedTime'],
+            "link": video['link']
 
+        }
