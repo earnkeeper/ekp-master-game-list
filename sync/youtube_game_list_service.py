848dcf480aa5d0525281a8ad0db9ecce933219db
Hikmat <hikmet.pirmamedov@mail.ru>
2022-06-22 08:56:36 +0400

    feat: reduce api calls by using channels info graber

--- sync/youtube_game_list_service.py
+++ sync/youtube_game_list_service.py
@@ -27,9 +27,9 @@ class YoutubeSyncService:
         self.youtube_api_service = youtube_api_service
 
     async def sync_youtube_games_info(self):
-        
+
         games = self.game_repo.find_all()
-        
+
         today_timestamp = get_midnight_utc(datetime.datetime.now()).timestamp()
 
         game_ids_with_videos_today = self.youtube_repo.find_game_ids_with_videos_today(today_timestamp)
@@ -37,9 +37,9 @@ class YoutubeSyncService:
         for game in games:
             if game['id'] in game_ids_with_videos_today:
                 continue
-            
+
             search_query = game['name']
-            
+
             if 'youtube_search_query' in game and game['youtube_search_query']:
                 search_query = game['youtube_search_query']
 
@@ -50,13 +50,14 @@ class YoutubeSyncService:
             videos = await self.get_youtube_game_videos_info(videos_id_list, game, today_timestamp, search_query)
 
             self.youtube_repo.save(videos)
-            
+
         self.youtube_repo.delete_where_timestamp_before(today_timestamp)
 
     async def get_youtube_game_videos_info(self, videos_id_list, game, today_timestamp, search_query):
         video_ids = []
+        channel_ids = []
         videos = []
-        
+
         for video_id in videos_id_list:
             video_ids.append(video_id['id']['videoId'])
 
@@ -64,24 +65,33 @@ class YoutubeSyncService:
         videos_info = await self.youtube_api_service.get_videos_info(video_ids_str)
 
         for video in videos_info:
+            channel_ids.append(video['snippet']['channelId'])
+            # channel_ids[video['id']] = video['snippet']['channelId']
             document = await self.get_single_video_info(video, game, today_timestamp, search_query)
             videos.append(document)
 
+        channel_ids_str = ','.join(channel_ids)
+        channels_info = await self.youtube_api_service.get_channels_info(channel_ids_str)
+
+        for id, channel_info in enumerate(channels_info):
+            videos[id]['subscribers_count'] = channel_info['statistics']['subscriberCount'] if 'subscriberCount' in \
+                                                                                               channel_info[
+                                                                                                   'statistics'] else None
+
         return videos
 
-
     async def get_single_video_info(self, video, game, today_timestamp, search_query):
-        channel_subs = await self.cache_service.wrap(
-            f"channelId_{video['snippet']['channelId']}",
-            lambda: self.youtube_api_service.get_channel_subs_count(video['snippet']['channelId']),
-            ex=3600
-        )
-        
+        # channel_subs = await self.cache_service.wrap(
+        #     f"channelId_{video['snippet']['channelId']}",
+        #     lambda: self.youtube_api_service.get_channel_subs_count(video['snippet']['channelId']),
+        #     ex=3600
+        # )
+
         view_count = None
-        
+
         if "statistics" in video and "viewCount" in video["statistics"]:
             view_count = video['statistics']['viewCount']
-            
+
         return {
             "id": video['id'],
             "search_query": search_query,
@@ -92,8 +102,10 @@ class YoutubeSyncService:
             "thumbnail": video['snippet']['thumbnails']['medium']['url'],
             "view_count": view_count,
             "duration": str(datetime.timedelta(seconds=isodate.parse_duration('PT12M23S').total_seconds())),
-            "publish_time": int(time.mktime(datetime.datetime.strptime(video['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%SZ").timetuple())),
+            "publish_time": int(time.mktime(
+                datetime.datetime.strptime(video['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%SZ").timetuple())),
             "channel_name": video['snippet']['channelTitle'],
-            "subscribers_count": channel_subs,
+            "channel_id": video['snippet']['channelId'],
+            # "subscribers_count": channel_subs,
             "link": f"https://www.youtube.com/watch?v={video['id']}"
         }
